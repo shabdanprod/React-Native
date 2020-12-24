9897847d3c65a62779873eb8e2540156197176dc
Shabdan <shabdan.prod@gmail.com>
2020-12-24 17:48:07 +0600

    Start

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ТАКО ИШТЕ</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+  
